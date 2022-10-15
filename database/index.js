8bea05c72798eabc577f789f57d018302db6b1b2
Marcos Gomes <69877839+MarcosGomesDev@users.noreply.github.com>
2022-10-15 18:21:58 -0300

    Delete database directory

--- database/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.URL)
-    .then(() => {
-        console.log('our db as connect')
-    })
-    .catch((err) => console.log(err))
