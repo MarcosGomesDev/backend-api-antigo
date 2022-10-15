e4b37aca936806596c6700e754686031794243bf
Eike Souza <eike2528@hotmail.com>
2022-10-15 17:34:05 -0300

    teste pre update 15do10

--- src/routes/sellerRoutes.js
+++ src/routes/sellerRoutes.js
@@ -7,16 +7,9 @@ const upload = require('../middlewares/uploadImage')
 const sellerRoutes = express.Router()
 
 // SELLER ROUTES
-<<<<<<< HEAD
-sellerRoutes.get('/sellers', isAuthSeller, seller.index) // RETURN SELLERS - // trocado o Auth pra Admin pq n entendi pq do seller buscar outros
-sellerRoutes.get('/seller', seller.seller) // RETURN SELLER
-sellerRoutes.get('/seller/products', seller.sellerProducts) // RETURN ONE SELLER AND HIS PRODUCTS
-// sellerRoutes.get('/seller', isAuthSeller, seller.logged) // RETURN LOGGED SELLER AND OUR PRODUCTS
-=======
 sellerRoutes.get('/sellers', seller.index) // RETURN SELLERS
 sellerRoutes.get('/seller/products', isAuthSeller, seller.productsBySeller)
 sellerRoutes.get('/seller/:id', seller.selected) // RETURN SELLER
->>>>>>> 4600e888e1429fb44e7aea939d8e4c5244820f8c
 sellerRoutes.post('/sign-up/seller', seller.register) // CREATE NEW SELLER
 sellerRoutes.post('/sign-in/seller', seller.login) // LOGIN SELLER
 sellerRoutes.post('/seller/forgot-password', seller.forgotPassword) // SEND LINK TO RESET PASSSWORD
